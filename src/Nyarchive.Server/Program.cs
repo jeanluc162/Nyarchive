1d5183ca1da19b922149da2c36e14cb0047413a1
Jean-Luc <jeanluc1@live.de>
2022-04-09 23:38:36 +0200

    Update Program.cs

--- src/Nyarchive.Server/Program.cs
+++ src/Nyarchive.Server/Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
 using Pomelo.EntityFrameworkCore.MySql;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<Nyarchive.Server.Model.NyarchiveDbContext>();
+builder.Configuration.AddJsonFile("appsettings.json");
+builder.Services.AddDbContext<Nyarchive.Server.Model.NyarchiveDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetSection("ConnectionStrings:Default").Value, MariaDbServerVersion.AutoDetect(builder.Configuration.GetSection("ConnectionStrings:Default").Value))
+);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
