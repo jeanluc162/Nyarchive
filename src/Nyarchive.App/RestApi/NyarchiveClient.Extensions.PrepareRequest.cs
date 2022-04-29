4041e69c150400a67512de863a397956e5877e95
Jean-Luc <jeanluc1@live.de>
2022-04-29 20:17:16 +0200

    Client generation for the app

---
+++ src/Nyarchive.App/RestApi/NyarchiveClient.Extensions.PrepareRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nyarchive.App.RestApi
+{
+    public partial class NyarchiveClient
+    {
+        private async Task PrepareRequestAsync(HttpClient client, HttpRequestMessage request, string url)
+        {
+            
+        }
+        private async Task PrepareRequestAsync(HttpClient client, HttpRequestMessage request, StringBuilder urlBuilder)
+        {
+
+        }
+    }
+}
---
+++ src/Nyarchive.App/RestApi/NyarchiveClient.Extensions.ProcessResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nyarchive.App.RestApi
+{
+    public partial class NyarchiveClient
+    {
+        private async Task ProcessResponseAsync(HttpClient client, HttpResponseMessage response, CancellationToken cancellationToken)
+        {
+
+        }
+    }
+}
